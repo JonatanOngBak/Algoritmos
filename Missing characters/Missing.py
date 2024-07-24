6aefb75226dfec492bbc56ee05a626f19d83ea5c
Jonatan Fernandes <jonathanfds2007@gmail.com>
2024-07-24 11:55:14 -0300

    Adicionado app.py & Atualizado

---
+++ Missing characters/Missing.py
@@ -0,0 +1,21 @@
+def main(str):
+    str_ = str
+    
+     
+    # Use a set to store present characters
+    present_chars = set()
+ 
+    # Add each character to the set
+    for char in str_:
+        if char.isalpha():
+            present_chars.add(char.lower())
+ 
+    # Check which characters are missing
+    missing_chars = "".join(c for c in "abcdefghijklmnopqrstuvwxyz" if c not in present_chars)
+ 
+    # Print the missing characters
+    if not missing_chars:
+        print("The string is a pangram.")
+    else:
+        print(missing_chars)
+
---
+++ Missing characters/app.py
@@ -0,0 +1,4 @@
+from Missing import main
+frase = str(input('Digite uma frase: '))  # The quick brown fox jumps over the dog
+main(frase) 
+                                          # a saida sera alyz
