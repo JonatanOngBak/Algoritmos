789701c32e77be04b60b9ea573c85fb7d7ed85ee
Jonatan Fernandes <jonathanfds2007@gmail.com>
2024-07-24 13:22:37 -0300

    Adicionado count.py

---
+++ Count/count.py
@@ -0,0 +1,6 @@
+"""Contar maiúsculas, minúsculas, caracteres especiais e valores numéricos
+Dada uma string, escreva um programa para contar a ocorrência de caracteres minúsculos
+caracteres maiúsculos, caracteres especiais e valores numéricos."""
+
+maiúsculas = minúsculas = numéricos = especiais = 0
+
