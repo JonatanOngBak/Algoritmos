1dd5bf932d0c941d1200f2fdbcb662ba8c152eb5
Jonatan Fernandes <jonathanfds2007@gmail.com>
2024-07-25 06:41:28 -0300

    Arquivo count Atualizado

--- Count/count.py
+++ Count/count.py
@@ -2,5 +2,18 @@
 Dada uma string, escreva um programa para contar a ocorrência de caracteres minúsculos
 caracteres maiúsculos, caracteres especiais e valores numéricos."""
 
-maiúsculas = minúsculas = numéricos = especiais = 0
 
+def Count(entrada):
+    maiúsculas = minúsculas = numéricos = especiais = 0
+    for i in range(len(entrada)):
+        if entrada[i].isupper():
+            maiúsculas += 1
+        elif entrada[i].islower():
+            minúsculas += 1
+        elif entrada[i].isnumeric():
+            numéricos += 1
+        else:
+            especiais += 1
+    print(f'maiúsculas {maiúsculas}\nminúsculas {minúsculas}\nnuméricos {numéricos}\nespeciais {especiais}')        
+
+            
